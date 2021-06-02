36e64197d4c32cd724c72d34d84f30dc2b9f0cd2
Rodrigues <fr.costa95@gmail.com>
2021-06-02 17:35:37 -0300

    allow mic

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -36,3 +36,24 @@ link.addEventListener('click', (event) => {
     event.preventDefault();
     download_file('../img/publish3.png', 'some_name');
 });
+
+function hasGetUserMedia() {
+// Note: Opera builds are unprefixed.
+return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+if (hasGetUserMedia()) {
+    // Good to go!
+
+    navigator.getUserMedia({audio:true}, 
+        function(stream) {
+            alert('we can access mic');
+        },
+        function(e) {
+          alert('Error capturing audio.');
+        }
+      );
+} else {
+    alert('getUserMedia() is not supported in your browser');
+}
